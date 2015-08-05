9509590d7ba48049d553769349a6638f8df41ce2
Hari Menon <imbleedingme@googlemail.com>
2015-08-05 08:54:16 -0400

    add a handler for HTML user agents

--- routes/common.js
+++ routes/common.js
@@ -4,6 +4,12 @@ var debug = require('debug')('calendar:routes:common');
 
 module.exports = {
   sendAppropriateResponse : function (req, res, output) {
+    if (req.accepts('html')) {
+      debug('sending html output');
+      res.send(output.text.replace(/(?:\r\n|\r|\n)/g, '<br />'));
+      return;
+    }
+
     if (req.accepts('text')) {
       debug('sending text output');
       res.send(output.text);
